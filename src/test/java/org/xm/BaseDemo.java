08faf38baddd7ab113c2566009e055c2b56eb321
yibao.wei <yibao.wei@qunar.com>
2018-08-11 17:45:28 +0800

    修改

--- src/test/java/org/xm/BaseDemo.java
+++ src/test/java/org/xm/BaseDemo.java
@@ -6,7 +6,7 @@ package org.xm;
 public class BaseDemo {
     public static void main(String[] args) {
         System.out.println("首次编译运行...");
-        double result = Similarity.cilinSimilarity("电动车","自行车");
+        double result = Similarity.conceptSimilarity("换一个pmo","updatepmo");
         System.out.println(result);
     }
 }
--- src/test/java/org/xm/word2vec/Word2vecTest.java
+++ src/test/java/org/xm/word2vec/Word2vecTest.java
@@ -17,6 +17,12 @@ public class Word2vecTest {
     private static final String RAW_CORPUS_SPLIT = "corpus/tianlongbabu.split.txt";
     private static final String RAW_CORPUS_SPLIT_MODEL = "corpus/tianlongbabu.split.txt.model";
 
+    public static final String RAW_QTALK_CORPUS = "qtalk/searchPmo.log";
+
+    private static final String RAW_QTALK_SPLIT = "qtalk/searchPmo.split.txt";
+    private static final String RAW_QTALK_SPLIT_MODEL = "qtalk/searchPmo.split.txt.model";
+
+
     @Test
     public void trainModel() throws Exception {
         HanLP.Config.ShowTermNature = false;// 关闭词性标注
@@ -26,9 +32,36 @@ public class Word2vecTest {
     }
 
     @Test
+    public void trainModelQtalk()throws Exception{
+        HanLP.Config.ShowTermNature = false;// 关闭词性标注
+        Tokenizer.fileSegment(SEGMENT, RAW_QTALK_CORPUS, RAW_QTALK_SPLIT);
+        String outputModelPath = Word2vec.trainModel(RAW_QTALK_SPLIT, RAW_QTALK_SPLIT_MODEL);
+
+        System.out.println("outputModelPath：" + outputModelPath);
+    }
+
+    @Test
+    public void testHomoionymQtalk() throws Exception {
+        List<String> result = Word2vec.getHomoionym(RAW_QTALK_SPLIT_MODEL, "pmo", 10);
+        System.out.println("pmo 近似词：" + result);
+    }
+
+    @Test
     public void testHomoionym() throws Exception {
         List<String> result = Word2vec.getHomoionym(RAW_CORPUS_SPLIT_MODEL, "武功", 10);
         System.out.println("武功 近似词：" + result);
+    }
+    @Test
+    public void testHomoionymNameQtalk()throws Exception{
+        String model = RAW_QTALK_SPLIT_MODEL;
+        List<String> result = Word2vec.getHomoionym(model, "pmo", 10);
+        System.out.println("pmo 近似词：" + result);
+
+        List<String> result2 = Word2vec.getHomoionym(model, "qunar", 10);
+        System.out.println("qunar 近似词：" + result2);
+
+        List<String> result3 = Word2vec.getHomoionym(model, "qtalk", 10);
+        System.out.println("qtalk 近似词：" + result3);
     }
 
     @Test
